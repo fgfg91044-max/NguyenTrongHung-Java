59719c6da8c597859b0f4450f7c9a5d1ac180c34
NguyenTrongHung-Java <fgfg91044@gmail.com>
2026-04-18 11:34:14 +0700

    bai moi

---
+++ HelloWorld/src/communityuni/com/New.java
@@ -0,0 +1,10 @@
+package communityuni.com;
+
+public class New {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chào các Thím!");
+	}
+
+}
